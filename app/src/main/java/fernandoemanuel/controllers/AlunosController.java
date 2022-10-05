272e854a627a626aaa1f76ac4857a8e8657aa7ec
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-10-05 17:55:14 +0000

    05-10

--- app/src/main/java/fernandoemanuel/controllers/AlunosController.java
+++ app/src/main/java/fernandoemanuel/controllers/AlunosController.java
@@ -1,8 +1,12 @@
 package fernandoemanuel.controllers;
 
+import java.sql.Date;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,5 +38,12 @@ public class AlunosController {
         aluno.setIdade(idade);
         alunosRepo.save(aluno);
         return "redirect:/alunos/list";
+    }
+
+    @RequestMapping("update/{id}")
+    public String update(Model model, @PathVariable int id) {
+        Optional<Aluno> aluno = alunosRepo.findById(id);
+        model.addAttribute("aluno", aluno.get());
+        return "update";
     }
 }
