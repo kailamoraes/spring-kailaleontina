b33299c2121b0cdf111155cc8e279bc912dfc152
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-10-17 17:10:36 +0000

    17-10

--- app/src/main/java/fernandoemanuel/controllers/AlunosController.java
+++ app/src/main/java/fernandoemanuel/controllers/AlunosController.java
@@ -44,6 +44,16 @@ public class AlunosController {
     public String update(Model model, @PathVariable int id) {
         Optional<Aluno> aluno = alunosRepo.findById(id);
         model.addAttribute("aluno", aluno.get());
-        return "update";
+        return "/alunos/update";
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public String saveUpdate(@RequestParam("nome") String nome, @RequestParam("idade") int idade, @RequestParam("id") int id ) {
+        Optional<Aluno> aluno = alunosRepo.findById(id);
+        aluno.get().setNome(nome);
+        aluno.get().setIdade(idade);
+        alunosRepo.save(aluno.get());
+        return "redirect:/alunos/list";
+
     }
 }
